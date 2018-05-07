1f500ae65b3aa709ed21260dca6bd6e09937b6b3
Roman Zhyla <webmaster.doger@gmail.com>
2018-05-07 17:30:38 +0300

    first step

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+// Функция плавного скроллинга якорных ссылок
+$('a[href^="#"]').click(function(){
+  var el = $(this).attr('href');
+  var destination = $(el).offset().top;
+  $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+  $('html').animate({ scrollTop: destination }, 1100);
+  return false;
+});
+
+/*
+ * Application
+ */
+
+$(document).tooltip({
+    selector: "[data-toggle=tooltip]"
+})
+
+/*
+ * Auto hide navbar
+ */
+jQuery(document).ready(function($){
+    var $header = $('.navbar-autohide'),
+        scrolling = false,
+        previousTop = 0,
+        currentTop = 0,
+        scrollDelta = 10,
+        scrollOffset = 150
+
+    $(window).on('scroll', function(){
+        if (!scrolling) {
+            scrolling = true
+
+            if (!window.requestAnimationFrame) {
+                setTimeout(autoHideHeader, 250)
+            }
+            else {
+                requestAnimationFrame(autoHideHeader)
+            }
+        }
+    })
+
+    function autoHideHeader() {
+        var currentTop = $(window).scrollTop()
+
+        // Scrolling up
+        if (previousTop - currentTop > scrollDelta) {
+            $header.removeClass('is-hidden')
+        }
+        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+            // Scrolling down
+            $header.addClass('is-hidden')
+        }
+
+        previousTop = currentTop
+        scrolling = false
+    }
+});
